dc6d80c0f5fbd29b01392443f20a149f9d59cce4
Dmitry Badanin <badanin.d.ni@sberbank.ru>
2023-04-17 15:55:17 +0300

    Interview

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [count, setCount] = useState<number>(0)
+  const [inputValue, setValue] = useState<string>('')
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <div><button onClick={() => setCount(count + 1)}>{count}</button></div>
+      <div><input type="text" value={inputValue} onChange={(e) => setValue(e.target.value)} /></div>
     </div>
   );
 }
