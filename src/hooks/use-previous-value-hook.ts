9ba791697626d75728f26e235795b0947489b50b
Alexander Kolpakov <3888406@gmail.com>
2023-04-18 11:57:41 +0300

    feat: add usePrevState hook

--- src/hooks/use-previous-value-hook.ts
+++ src/hooks/use-previous-value-hook.ts
@@ -1,1 +1,14 @@
-export const usePreviousValue = ()=> {}
+import { useRef, useEffect, Dispatch, SetStateAction } from 'react';
+
+function usePrevState<S>(state: [S, Dispatch<SetStateAction<S>>]): [S, Dispatch<SetStateAction<S>>,  S | undefined] {
+    const ref = useRef<S>();
+    const [stateValue, stateDispatch] = state;
+
+    useEffect(() => {
+        ref.current = stateValue;
+    },[stateValue]);
+
+    return [stateValue, stateDispatch, ref.current];
+}
+
+export default usePrevState;
